d1cb3f35ff04cb710feee682fbcf256796adcb03
John Van Note <johnlvannote@gmail.com>
2019-03-09 12:46:34 -0500

    Initial commit

---
+++ src/main/java/com/jvn/IntegerToWords.java
@@ -0,0 +1,169 @@
+package com.jvn;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Converts integers to the English equivalent
+ */
+public class IntegerToWords {
+
+  private IntegerToWords(){}
+
+  /**
+   * Converts an integer to its English equivalent.
+   * @param number The integer.
+   * @return The English equivalent.
+   */
+  public static String toWords(int number) {
+    int numberCopy = number;
+    Map<Integer, Integer> splitNumber = new TreeMap<>();
+
+    for (int i=0; numberCopy != 0; i++) {
+      splitNumber.put(i, numberCopy % 1000);
+      numberCopy = numberCopy / 1000;
+    }
+
+    Deque<String> deque = new ArrayDeque<>();
+    int splitNumLength = splitNumber.size();
+    for (int i=0; i<splitNumLength; i++) {
+      int digits = splitNumber.get(i);
+
+      // Lets get the place
+      if (digits != 0) {
+        String placeWord = toWordPlace(i);
+        updateDeque(deque, placeWord);
+      }
+
+      // Lets see if we can take care of the tens and ones at once
+      int tensNumber = digits % 100;
+      String tensWord = toWordBase(tensNumber);
+
+      if (tensWord != null) {
+        updateDeque(deque, tensWord);
+      } else {
+
+        // Now for the ones
+        int onesDigit = digits % 10;
+        String onesWord = toWordBase(onesDigit);
+        updateDeque(deque, onesWord);
+
+        // Lets get the second one and remove the remainder
+        int tensNumberNoRemainder = (digits % 100) / 10 * 10;
+        String tensWordNoRemainder = toWordBase(tensNumberNoRemainder);
+        updateDeque(deque, tensWordNoRemainder);
+
+      }
+
+      // Lets get the first digit
+      int hundredsDigit = digits / 100;
+      String hundredsWord = toWordBase(hundredsDigit);
+      if (!"".equals(hundredsWord)) updateDeque(deque, "hundred");
+      updateDeque(deque, hundredsWord);
+    }
+
+    String dequeString = dequeToString(deque);
+    return dequeString.isEmpty() ? "zero" : dequeString;
+  }
+
+  /**
+   * Converts words to string base.
+   * @param number The number to be converted.
+   * @return The converted number.
+   */
+  private static String toWordBase(int number) {
+    int min = 0;
+    int max = 99;
+    if (number < min || number > max)
+      throw new IllegalArgumentException(String.format("Input argument number must be in range [%d, %d].", min, max));
+
+    Map<Integer, String> base = new HashMap<>();
+    base.put(0, "");
+    base.put(1, "one");
+    base.put(2, "two");
+    base.put(3, "three");
+    base.put(4, "four");
+    base.put(5, "five");
+    base.put(6, "six");
+    base.put(7, "seven");
+    base.put(8, "eight");
+    base.put(9, "nine");
+    base.put(10, "ten");
+    base.put(11, "eleven");
+    base.put(12, "twelve");
+    base.put(13, "thirteen");
+    base.put(14, "fourteen");
+    base.put(15, "fifteen");
+    base.put(16, "sixteen");
+    base.put(17, "seventeen");
+    base.put(18, "eighteen");
+    base.put(19, "nineteen");
+    base.put(20, "twenty");
+    base.put(30, "thirty");
+    base.put(40, "forty");
+    base.put(50, "fifty");
+    base.put(60, "sixty");
+    base.put(70, "seventy");
+    base.put(80, "eighty");
+    base.put(90, "ninety");
+
+    return base.get(number);
+  }
+
+  /**
+   * Converts words to string place.
+   * @param number The number to be converted.
+   * @return The converted number.
+   */
+  private static String toWordPlace(int number) {
+    int min = 0;
+    int max = 9;
+    if (number < min || number > max)
+      throw new IllegalArgumentException(String.format("Input argument number must be in range [%d, %d].", min, max));
+
+    Map<Integer, String> place = new HashMap<>();
+    place.put(0, "");
+    place.put(1, "thousand");
+    place.put(2, "million");
+    place.put(3, "billion");
+    place.put(4, "trillion");
+    place.put(5, "quadrillion");
+    place.put(6, "quintillion");
+    place.put(7, "sextillion");
+    place.put(8, "septillion");
+    place.put(9, "octillion");
+
+    return place.get(number);
+  }
+
+  /**
+   * Converts a deque to a readable number string.
+   * @param deque The deque.
+   * @return The readable string.
+   */
+  private static String dequeToString(Deque<String> deque) {
+    StringBuilder string = new StringBuilder();
+
+    Iterator<String> it = deque.iterator();
+    while (it.hasNext()) {
+      string.append(it.next());
+      if (it.hasNext()) string.append(" ");
+    }
+
+    return string.toString();
+  }
+
+  /**
+   * Updates a deque but ignores all the empty string and null string.
+   * @param deque The deque.
+   * @param elem The element to be updated.
+   */
+  private static void updateDeque(Deque<String> deque, String elem) {
+    if (elem != null && !elem.isEmpty()) deque.push(elem);
+  }
+
+}
---
+++ src/test/java/com/jvn/IntegerToWords_UnitTest.java
@@ -0,0 +1,76 @@
+package com.jvn;
+
+import static com.jvn.IntegerToWords.toWords;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class IntegerToWords_UnitTest {
+
+  @DataProvider
+  public Object[][] numbas() {
+    return new Object[][]{
+        {0, "zero"},
+        {1, "one"},
+        {2, "two"},
+        {3, "three"},
+        {4, "four"},
+        {5, "five"},
+        {6, "six"},
+        {7, "seven"},
+        {8, "eight"},
+        {9, "nine"},
+        {10, "ten"},
+        {11, "eleven"},
+        {12, "twelve"},
+        {13, "thirteen"},
+        {14, "fourteen"},
+        {15, "fifteen"},
+        {16, "sixteen"},
+        {17, "seventeen"},
+        {18, "eighteen"},
+        {19, "nineteen"},
+        {20, "twenty"},
+        {21, "twenty one"},
+        {30, "thirty"},
+        {32, "thirty two"},
+        {40, "forty"},
+        {43, "forty three"},
+        {50, "fifty"},
+        {54, "fifty four"},
+        {60, "sixty"},
+        {65, "sixty five"},
+        {70, "seventy"},
+        {76, "seventy six"},
+        {80, "eighty"},
+        {87, "eighty seven"},
+        {90, "ninety"},
+        {98, "ninety eight"},
+        {100, "one hundred"},
+        {101, "one hundred one"},
+        {103, "one hundred three"},
+        {107, "one hundred seven"},
+        {109, "one hundred nine"},
+        {113, "one hundred thirteen"},
+        {127, "one hundred twenty seven"},
+        {131, "one hundred thirty one"},
+        {1000, "one thousand"},
+        {2500, "two thousand five hundred"},
+        {3750, "three thousand seven hundred fifty"},
+        {4875, "four thousand eight hundred seventy five"},
+        {10000, "ten thousand"},
+        {100000, "one hundred thousand"},
+        {1000000, "one million"},
+        {10000000, "ten million"},
+        {100000000, "one hundred million"},
+        {1000000000, "one billion"},
+    };
+  }
+
+  @Test(dataProvider = "numbas")
+  public void toWords_Numbers_Words(int n, String s) {
+    Assert.assertEquals(toWords(n), s);
+  }
+
+}
